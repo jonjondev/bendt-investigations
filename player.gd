53ae2f8dd5ca90d6c619b2390447fb2f2fbcc577
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-27 18:11:35 +1000

    Added variable and function type-hinting to player script

--- player.gd
+++ player.gd
@@ -1,14 +1,16 @@
 extends KinematicBody
 
-onready var ray_cast_group = $RayCasts
-onready var ray_casts = $RayCasts.get_children()
-var move_speed = 0.05
-var gravity = Vector3(0, -10, 0)
-var velocity = Vector3()
-var extent_offset = 0.25
+var move_speed : float = 0.05
+var extent_offset : float = 0.25
+var gravity : Vector3 = Vector3(0, -10, 0)
+var velocity : Vector3 = Vector3.ZERO
 
-func _physics_process(delta):
-	var move = Vector3.ZERO
+onready var ray_cast_group : Spatial = $RayCasts
+onready var ray_casts : Array = $RayCasts.get_children()
+
+
+func _physics_process(delta) -> void:
+	var move : Vector3 = Vector3.ZERO
 	if Input.is_action_pressed("ui_left"):
 		move.x = -1
 	if Input.is_action_pressed("ui_up"):
@@ -24,7 +26,8 @@ func _physics_process(delta):
 	velocity += gravity * delta 
 	velocity = move_and_slide(velocity)
 
-func get_valid_move(move):
+
+func get_valid_move(move) -> Vector3:
 	if is_on_surface(move):
 		return move
 	elif is_on_surface(Vector3(move.x, 0, 0)):
@@ -33,7 +36,8 @@ func get_valid_move(move):
 		return Vector3(0, 0, move.z)
 	return Vector3.ZERO
 
-func is_on_surface(potential_translation):
+
+func is_on_surface(potential_translation) -> bool:
 	ray_cast_group.translation = potential_translation
 	for ray in ray_casts:
 		ray.force_raycast_update()
