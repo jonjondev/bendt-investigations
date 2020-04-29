dd6d4146633a0000bcdf1b275ba53d0f37c6aa90
Jonathan Moallem <jdmoallem@gmail.com>
2020-04-29 18:27:08 +1000

    Moved camera into its own scene and made it follow the player's movement on the x-axis

---
+++ src/Camera.gd
@@ -0,0 +1,10 @@
+extends Camera
+
+export (NodePath) var player
+
+func _ready():
+	player = get_node(player)
+
+func _physics_process(delta):
+	if abs(translation.x - player.translation.x) > 0.1:
+		translation.x = lerp(translation.x, player.translation.x, delta)
