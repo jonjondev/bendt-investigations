adb76c463b6bdec90a4daaf66b23f97e96912bb2
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-02 13:32:03 +1000

    Allowed the user to interact with 3D text as an editor tool through the root Spatial node with text setter

--- src/Scripts/3DText.gd
+++ src/Scripts/3DText.gd
@@ -1,5 +1,7 @@
+tool
 extends Spatial
 
+export (String) var text: String setget set_text
 export (bool) var is_billboard: bool
 
 func _process(_delta):
@@ -8,3 +10,9 @@ func _process(_delta):
 		var look: Vector3 = camera.to_global(Vector3(0, 0, -100)) - camera.global_transform.origin
 		look_at(translation + look, Vector3.UP)
 		rotate_object_local(Vector3.BACK, camera.rotation.z)
+
+func set_text(value):
+	text = value
+	if not is_inside_tree():
+		yield(self, 'ready')
+	$Viewport/Control/Label.text = text
