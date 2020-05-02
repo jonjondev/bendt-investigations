5efe5a916462faec818f647a9289f912b6b5984e
Jonathan Moallem <jdmoallem@gmail.com>
2020-05-02 13:10:01 +1000

    Added billboard flag to 3D text to turn feature on and off

--- src/Scripts/3DText.gd
+++ src/Scripts/3DText.gd
@@ -1,7 +1,10 @@
 extends Spatial
 
+export (bool) var is_billboard: bool
+
 func _process(_delta):
-	var camera = get_viewport().get_camera()
-	var look = camera.to_global(Vector3(0, 0, -100)) - camera.global_transform.origin
-	look_at(translation + look, Vector3.UP)
-	rotate_object_local(Vector3.BACK, camera.rotation.z)
+	if is_billboard:
+		var camera: Camera = get_viewport().get_camera()
+		var look: Vector3 = camera.to_global(Vector3(0, 0, -100)) - camera.global_transform.origin
+		look_at(translation + look, Vector3.UP)
+		rotate_object_local(Vector3.BACK, camera.rotation.z)
