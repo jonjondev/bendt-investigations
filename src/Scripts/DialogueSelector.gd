678e83452b3336a16d2a0886303dd6650745a3c9
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-07 23:35:41 +1000

    Added the ability to adjust spacing between items in dialogue selector

--- src/Scripts/DialogueSelector.gd
+++ src/Scripts/DialogueSelector.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 export (int) var faces = 0
+export (float) var spacing = 0.05
 
 var text_scene = preload("res://src/3DText.tscn")
 var texts = []
@@ -12,6 +13,7 @@ func _ready():
 		new_text.text = "Line " + str(i)
 		texts.append(new_text)
 		new_text.rotation_degrees = Vector3(i * rotation_angle, 0, 0)
+		new_text.translate(Vector3(0, 0, spacing * (faces/2)))
 		add_child(new_text)
 
 func _physics_process(delta):
