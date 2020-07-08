58b37a0dd20b3838345aa5e0b1b8ed2b1a31fb56
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-08 17:17:20 +1000

    Made dialogue selector populate itself with dialogue lines

--- src/Scripts/DialogueSelector.gd
+++ src/Scripts/DialogueSelector.gd
@@ -1,23 +1,24 @@
 extends Spatial
 
-export (int) var faces = 0
+export (Array) var lines = []
 export (float) var spacing = 0.05
+export (float) var scroll_speed = 0.5
 
 var text_scene = preload("res://src/3DText.tscn")
 var texts = []
 
 func _ready():
-	var rotation_angle: float = exterior_angles(faces)
-	for i in range(faces):
+	var rotation_angle: float = exterior_angles(lines.size())
+	for i in range(lines.size()):
 		var new_text = text_scene.instance()
-		new_text.text = "Line " + str(i)
+		new_text.text = lines[i]
 		texts.append(new_text)
 		new_text.rotation_degrees = Vector3(i * rotation_angle, 0, 0)
-		new_text.translate(Vector3(0, 0, spacing * (faces/2)))
+		new_text.translate(Vector3(0, 0, spacing * (lines.size()/2)))
 		add_child(new_text)
 
 func _physics_process(delta):
-	rotate_x(delta * 0.5)
+	rotate_x(delta * -scroll_speed)
 
 func exterior_angles(n: float) -> float:
 	return 360.0/n
