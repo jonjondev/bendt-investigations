b41a245176bf6cd4d12582f66bae77947cb9fa99
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-10 18:09:22 +1000

    Made dialogue selector use custom curve for rotation degree

--- src/Scripts/DialogueSelector.gd
+++ src/Scripts/DialogueSelector.gd
@@ -2,11 +2,14 @@ extends Spatial
 
 export (Array) var lines = []
 export (float) var spacing = 0.05
-export (float) var scroll_speed = 0.1
+export (float) var scroll_speed = 1.0
 
 var text_scene: PackedScene = preload("res://src/3DText.tscn")
 var texts: Array = []
-var rotate_to = 0
+var selected_item = 0
+var rotate_to = null
+var rotation_amount = 0
+var curve = Curve.new()
 
 onready var rotation_angle: float = exterior_angles(lines.size())
 
@@ -18,22 +21,31 @@ func _ready():
 		new_text.rotation_degrees = Vector3(i * rotation_angle, 0, 0)
 		new_text.translate(Vector3(0, 0, spacing * (lines.size()/2)))
 		add_child(new_text)
+	curve.add_point(Vector2(0.0, 0.0))
+	curve.add_point(Vector2(0.6, 0.8))
+	curve.add_point(Vector2(0.5, 0.2))
+	curve.add_point(Vector2(1.0, 1.0))
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_accept"):
-		if not rotate_to:
-			rotate_to = rotation_degrees.x + rotation_angle
-		else:
-			rotate_to = rotate_to + rotation_angle
+	if Input.is_action_just_pressed("ui_up"):
+		move_selection(1)
+	if Input.is_action_just_pressed("ui_down"):
+		move_selection(-1)
+
+func move_selection(direction: int):
+	selected_item = (selected_item + direction) % lines.size()
+	rotate_to = rotation_angle * selected_item
+	rotation_amount = 0
 
 func _physics_process(delta):
-	if rotate_to != 0:
-		if abs(rotation_degrees.x - rotate_to) > 1.0:
-			var new_rotation = rotation_degrees.x + (rotate_to - rotation_degrees.x) * delta
-			rotation_degrees.x = new_rotation + 1.0
+	if rotate_to != null:
+		if rotation_amount < 1.0:
+			var new_rotation = rotation_degrees.x + (rotate_to - rotation_degrees.x) * curve.interpolate(rotation_amount)
+			rotation_degrees.x = new_rotation
+			rotation_amount += delta * scroll_speed
 		else:
 			rotation_degrees.x = rotate_to
-			rotate_to = 0
+			rotate_to = null
 
 func exterior_angles(n: float) -> float:
 	return 360.0/n
