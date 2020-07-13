137d97e1e1190b7b2f88ba9d7644a73509f877ac
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-13 16:13:02 +1000

    Updated 3DText to broadly cover needs of text fading through alpha

--- src/Scripts/3DText.gd
+++ src/Scripts/3DText.gd
@@ -4,8 +4,11 @@ extends Spatial
 export (Color) var base_colour: Color
 export (String) var text: String setget set_text
 export (bool) var is_billboard: bool
+export (float) var base_alpha: float = 1.0
 
+func _ready():
 	$Viewport/Control/Label.modulate = base_colour
+
 func _process(_delta):
 	if is_billboard:
 		var camera: Camera = get_viewport().get_camera()
@@ -13,8 +16,15 @@ func _process(_delta):
 		look_at(translation + look, Vector3.UP)
 		rotate_object_local(Vector3.BACK, camera.rotation.z)
 
-func set_text(value):
+func set_text(value) -> void:
 	text = value
 	if not is_inside_tree():
 		yield(self, 'ready')
 	$Viewport/Control/Label.text = text
+
+func set_alpha(alpha: float) -> void:
+	$Viewport/Control/Label.modulate = setget_alpha(base_colour, base_alpha + alpha)
+
+func setget_alpha(colour: Color, alpha: float) -> Color:
+	colour.a = alpha
+	return colour
