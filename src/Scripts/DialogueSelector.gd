748c48b7358b16d4ca34222935b56b8d27834b8a
Jonathan Moallem <jdmoallem@gmail.com>
2020-07-13 16:19:04 +1000

    Fixed dialogue selector rotation highlighting and exposed tweaking values to editor

--- src/Scripts/DialogueSelector.gd
+++ src/Scripts/DialogueSelector.gd
@@ -8,29 +8,31 @@ export (String) var line_2
 export (String) var line_3
 export (float) var spacing = 0.05
 export (float) var scroll_speed = 1.0
+export (Color) var text_colour
+export (float) var text_fade = 0.5
 export (Curve) var scroll_curve
 
 var text_scene: PackedScene = preload("res://src/3DText.tscn")
-var text_positions: Array = []
+var texts: Dictionary = {}
 var selected_item: int = 0
 var rotation_amount: float = 0
 var rotate_to = null
 
-onready var texts: Array = [line_1, line_2, line_3]
 onready var rotation_angle: float = exterior_angles(sides)
 onready var player: Node = $"../../Player"
 
 func _ready():
+	var text_content: Array = [line_1, line_2, line_3]
 	for i in range(lines):
 		var new_text = text_scene.instance()
-		new_text.text = texts[i]
+		new_text.text = text_content[i]
+		new_text.base_colour = text_colour
+		new_text.base_alpha = text_fade
 		new_text.rotation_degrees = Vector3(i * rotation_angle, 0, 0)
-		text_positions.append(i * rotation_angle)
 		new_text.translate(Vector3(0, 0, spacing * (sides/2.0)))
+		texts[new_text] = i * rotation_angle - (lines-1) * rotation_angle
 		add_child(new_text)
-	move_selection(1)
-	rotation_degrees.x = rotate_to
-	rotate_to = null
+	move_selection(0)
 
 func _input(_event):
 	if player.in_action:
@@ -55,6 +57,8 @@ func _physics_process(delta):
 			var new_rotation = rotation_degrees.x + (rotate_to - rotation_degrees.x) * scroll_curve.interpolate(rotation_amount)
 			rotation_degrees.x = new_rotation
 			rotation_amount += delta * scroll_speed
+			for i in range(texts.size()):
+				texts.keys()[i].set_alpha(rotation_amount if i == (2 - selected_item) else 0.0)
 
 func exterior_angles(n: float) -> float:
 	return 360.0/n
